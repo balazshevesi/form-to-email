30586808b13a12658a2e9cdb1caaf31ba3e616fc
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2024-03-10 15:48:12 +0100

    Initial implementation

---
+++ my-app/src/index.tsx
@@ -0,0 +1,62 @@
+import { Button, Html, Tailwind } from "@react-email/components";
+import { render } from "@react-email/render";
+import { Hono } from "hono";
+
+const nodemailer = require("nodemailer");
+
+const app = new Hono();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+const TailwindWrapper = ({ children }: { children?: any }) => {
+  return <Tailwind>{children} </Tailwind>;
+};
+
+const Table = ({ data }: { data: any }) => {
+  const arrayOfKeyValuePairs = [];
+
+  Object.keys(data).forEach((key, i) => {
+    if (!key.startsWith("_"))
+      arrayOfKeyValuePairs.push({ key, value: data[key] });
+  });
+
+  return (
+    <TailwindWrapper>
+      {arrayOfKeyValuePairs.map((item, i) => (
+        <div>
+          {item.key}: {item.value}
+        </div>
+      ))}
+    </TailwindWrapper>
+  );
+};
+
+app.post("/:sendToEmail", async (c) => {
+  const sendToEmail = c.req.param("sendToEmail");
+
+  const body =
+    c.req.header("Content-Type") === "application/json"
+      ? await c.req.json()
+      : await c.req.parseBody();
+
+  const mailOptions = {
+    from: process.env.EMAIL_ADDRESS,
+    to: sendToEmail,
+    subject: body._subject || "untitled",
+    html: render(<Table data={body} />),
+  };
+  await transporter.sendMail(mailOptions);
+
+  return c.text("success!");
+});
+
+export default {
+  port: process.env.PORT,
+  fetch: app.fetch,
+};
